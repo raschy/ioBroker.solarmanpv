0804ec15fbfbb38e470cfa7f7f341a3c835a172f
raschy <raschy@gmx.de>
2022-06-30 12:15:53 +0200

    User warnings addet

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ const utils = require('@iobroker/adapter-core');
 const axios = require('axios').default;
 const crypto = require('crypto-js/sha256');
 
+this.tmpStationId = null;
+this.stationId = null;
+this.deviceId = null;
+this.deviceSn = null;
+this.hash = null;
+this.token = null;
+
 class Solarmanpv extends utils.Adapter {
 
 	/**
@@ -27,15 +34,9 @@ class Solarmanpv extends utils.Adapter {
 		this.on('ready', this.onReady.bind(this));
 		this.on('unload', this.onUnload.bind(this));
 		//
-		this.baseUrl = 'https://api.solarmanpv.com';
-		this.stationId = null;
-		this.deviceId = null;
-		this.deviceSn = null;
-		this.hash = null;
-		this.token = null;
-		//
 		this.maxGetToken = 1;
 		//
+		this.baseUrl = 'https://api.solarmanpv.com';
 		this.http = axios.create({
 			baseURL: this.baseUrl,
 			timeout: 3000,
@@ -92,47 +93,56 @@ class Solarmanpv extends utils.Adapter {
 		this.log.debug(`[onReady] started`);
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.debug('config email: ' + this.config.email);
-		this.log.debug('config password: ' + this.config.password);
-		this.log.debug('config appId: ' + this.config.appId);
-		this.log.debug('config appSecret: ' + this.config.appSecret);
 
-		this.hash = crypto(this.config.password).toString();
-		this.log.debug('intern hash: ' + this.hash);
+		console.log('==== Start ====');
+
+		if (!this.config.email || !this.config.password) {
+			this.log.error(`User email and/or user password empty - please check instance configuration`);
+			return;
+		}
+
+		if (!this.config.appId || !this.config.appSecret) {
+			this.log.error(`Solarman APP ID and/or APP Secrets empty - please check instance configuration`);
+			return;
+		}
 
 		const object = await this.getForeignObjectAsync('system.adapter.solarmanpv');
 		if (typeof(object) != 'undefined' && object != null){
 			this.token = object.native.aktiveToken;
 			this.log.debug('intern token: ' + this.token);
 		}
-		console.log('==== Start ====');
 
 		try {
 			// add bearer token to header of axios instance for next requests
 			this.http.defaults.headers.common['Authorization'] = 'bearer ' + this.token;
 
 			// get station-id via api-call
-			this.stationId = await this.getStationId();
-			this.log.info('Station ID: ' + this.stationId);
+			this.tmpStationID = await this.getStationId();
+			if (typeof(this.tmpStationID) != 'undefined' && this.tmpStationID != null){
+				this.stationId = this.tmpStationID;
+				this.log.info('Station ID: ' + this.stationId);
+			} else {
+				this.log.warn('no valid station ID found.');
+				return;
+			}
 
-			// get device-sn  & device-id via api-call
-			this.deviceSn = await this.getInverterId();
-			this.log.info('Device SN: ' + this.deviceSn);
-			this.log.info('Device ID: ' + this.deviceId);
+			// get device-id/sn via api-call
+			await this.getInverterId();
 
 
 			// get data from station via api-call
 			await this.getStationData().then(result =>
 				this.updateStationData(result));
 
+			/*
 			// get data from device via api-call
 			await this.getDeviceData().then(result =>
 				this.updateDeviceData(result));
+			*/
 
-			/*
 			await this.getRealTimeData().then(result =>
 				this.updateDeviceData(result));
-			*/
+
 		}
 		catch (err) {
 			this.log.error(`[onReady] error: ${err}`);
@@ -208,19 +218,18 @@ class Solarmanpv extends utils.Adapter {
 
 	// update station data in ioBroker
 	updateStationData(data) {
-		//console.log('Station Data: ', data);
 		// define keys that shall be updated
 		const updateKeys = ['generationPower', 'lastUpdateTime'];
 
 		updateKeys.forEach((key) => {
 			//this.log.info('[updateStationData] '+ key + ' Data: ' + data[key]);
 			if (key == 'generationPower') {
-				this.log.info('[updateStationData] generationPower : '+ data[key]);
+				//this.log.info('[updateStationData] generationPower : '+ data[key]);
 				this.fillSensorData('StationData', key, key, data[key],'W');
 			}
 			if (key == 'lastUpdateTime') {
 				const lastUpdate = new Date(data[key]*1000);
-				this.log.info('[updateStationData] lastUpdateTime : '+ lastUpdate);
+				//this.log.info('[updateStationData] lastUpdateTime : '+ lastUpdate);
 				this.fillSensorData('StationData', key, key, lastUpdate,'');
 			}
 		});
@@ -228,13 +237,11 @@ class Solarmanpv extends utils.Adapter {
 
 	// update inverter data in ioBroker
 	updateDeviceData(data) {
-		//console.log('Inverter Data: ', data);
 		// define keys that shall be updated (works in dataList only)
 		const updateKeys = ['DV1','DV2','DC1','DC2','DP1','DP2','AV1','Et_ge0','Etdy_ge0','AC_RDT_T1','APo_t1'];
 		const values = data.dataList.filter((obj) => updateKeys.includes(obj.key));
 		values.forEach((obj) => {
-			//console.log(obj.value);
-			this.log.info('[updateDeviceData] '+ obj.key + ' Data: ' + obj.value + ' Unit: ' + obj.unit + ' Name: ' + obj.name);
+			//this.log.info('[updateDeviceData] '+ obj.key + ' Data: ' + obj.value + ' Unit: ' + obj.unit + ' Name: ' + obj.name);
 			this.fillSensorData('DeviceData', obj.key, obj.name, obj.value, obj.unit);
 		});
 	}
@@ -254,16 +261,10 @@ class Solarmanpv extends utils.Adapter {
 				}
 			)
 			.then((response) => {
-				// this.log.error(`[getDeviceData] msg: ${response.data.msg}`);
-				if(response.data.msg === 'auth invalid token') return Promise.reject('Invalid-Token');		//throw 'InvalidToken';
 				return response.data;
 			})
 			.catch(function (error) {
 				self.log.error(`[getDeviceData] error: ${error}`);
-				//console.log('=== Request ===');
-				//console.log(error.request);
-				//console.log('=== Response ===');
-				//console.log(error);
 			});
 	}
 
@@ -301,17 +302,9 @@ class Solarmanpv extends utils.Adapter {
 				}
 			)
 			.then((response) => {
-				//console.log('RealTimeData ID ====');
-				//console.log(response.data);
-
-				//const deviceSn = response.data.deviceListItems[0].deviceSn;
-				//console.log(deviceSn);
-				//if(response.data.msg === 'auth invalid token') return Promise.reject('Invalid-Token');
-				//this.log.debug(`[getRealTimeData] Device SN <: ${response.data.deviceListItems[0].deviceSn}`);
 				return response.data;
 			})
 			.catch(function (error) {
-				console.log(error.request);
 				self.log.error(`[getRealTimeData] error: ${error}`);
 			});
 	}
@@ -332,20 +325,13 @@ class Solarmanpv extends utils.Adapter {
 				}
 			)
 			.then((response) => {
-				//console.log('Inverter ID ====');
-				//console.log(response.data);
-				//const deviceSn = response.data.deviceListItems[0].deviceSn;
-				//console.log(deviceSn);
-				//if(response.data.msg === 'auth invalid token') return Promise.reject('Invalid-Token');
-
-				this.deviceId = response.data.deviceListItems[0].deviceId;
-				this.log.debug(`[getInverterId] Device ID <: ${response.data.deviceListItems[0].deviceId}`);
-
 				this.log.debug(`[getInverterId] Device SN <: ${response.data.deviceListItems[0].deviceSn}`);
-				return response.data.deviceListItems[0].deviceSn;
+				this.log.debug(`[getInverterId] Device ID <: ${response.data.deviceListItems[0].deviceId}`);
+				this.deviceSn = response.data.deviceListItems[0].deviceSn;
+				this.deviceId = response.data.deviceListItems[0].deviceId;
+				return;
 			})
 			.catch(function (error) {
-				console.log(error.request);
 				self.log.error(`[getInverterId] error: ${error}`);
 			});
 	}
@@ -362,20 +348,13 @@ class Solarmanpv extends utils.Adapter {
 				}
 			)
 			.then((response) => {
-				//console.log('Station ID ====');
-				//console.log(response.data);
-
 				//const total = response.data.total;	// Anzahl der Plants
 				const objStationList = response.data.stationList;
 				const stationId = objStationList[0].id;
-
-				this.log.debug(`[getStationId] StationList <: ${stationId}`);
-				//this.log.info(`[getStationId] msg: ${response.data.msg.stationList.id}`);
-				//if(response.data.msg === 'auth invalid token') return Promise.reject('Invalid-Token');
+				this.log.debug(`[getStationId] data <: ${stationId}`);
 				return stationId;
 			})
 			.catch(function (error) {
-				console.log(error.request);
 				self.log.error(`[getStationId] error: ${error}`);
 			});
 	}
@@ -383,12 +362,14 @@ class Solarmanpv extends utils.Adapter {
 	// get Token from api
 	async getToken() {
 		const self = this;
+		this.log.debug('[getToken] config email: ' + this.config.email);
+		this.log.debug('[getToken] config password: ' + this.config.password);
+		this.log.debug('[getToken] config appId: ' + this.config.appId);
+		this.log.debug('[getToken] config appSecret: ' + this.config.appSecret);
+		//	Hashwwert (sha256) des Passworts erzeugen
+		this.hash = crypto(this.config.password).toString();
+		this.log.debug('[getToken] intern hash: ' + this.hash);
 
-		console.log(`[getToken] ==== neues ====`);
-		console.log(`[getToken] appId: ${this.config.appId}`);
-		console.log(`[getToken] appSecret: ${this.config.appSecret}`);
-		console.log(`[getToken] email: ${this.config.email}`);
-		console.log(`[getToken] hash: ${this.hash}`);
 		return await this.http
 			.post('/account/v1.0/token?appId=' + this.config.appId + '&language=en', {
 				appSecret: this.config.appSecret,
@@ -396,14 +377,10 @@ class Solarmanpv extends utils.Adapter {
 				password: this.hash,
 			})
 			.then((response) => {
-				console.log('TOKEN ====');
-				console.log(response);
 				self.log.debug(`[getToken] debug: ${response.data.access_token}`);
 				return response.data.access_token;
 			})
 			.catch(function (error) {
-				//console.log(error.request);
-				console.log(error.config);
 				self.log.error(`[getToken] error: ${error}`);
 			});
 	}
