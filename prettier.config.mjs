836869080472d27507553eb2783d346e49311c9b
raschy <raschy@gmx.de>
2025-02-06 10:28:41 +0100

    adapter-core 3.2.3

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,5 +4,5 @@ import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
 export default {
 	...prettierConfig,
 	// uncomment next line if you prefer double quotes
-	// singleQuote: false,
+	singleQuote: true,
 }
